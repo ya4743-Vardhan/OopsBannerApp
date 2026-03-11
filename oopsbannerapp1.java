42b9b50ea666539e62dc4f700e933a1ac6838608
Vardhan <vardhan@Vardhans-MacBook-Air-357.local>
2026-03-11 14:59:22 +0530

    Added all 6 assignment versions

---
+++ oopsbannerapp1.java
@@ -0,0 +1,1 @@
+public class oopsbannerapp1 { public static void main(String[] args) { System.out.println("OOPS"); } }
---
+++ oopsbannerapp4.java
@@ -0,0 +1,1 @@
+public class oopsbannerapp4 { public static void main(String[] args) { String[] bannerLines = { " ** ** ** ** ", "* * * * * * * * ", "* * * * ** ** ", "* * * * * ", " ** ** * ***" }; for (String line : bannerLines) { System.out.println(line); } } }
